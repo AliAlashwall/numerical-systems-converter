1847387bc2eeec0911689157fba8ed9c6e14521c
Ali <alialashwall14@gmail.com>
2023-05-13 16:25:47 +0300

    commit

--- app/src/main/java/com/example/number_systems_converter/MainActivity.kt
+++ app/src/main/java/com/example/number_systems_converter/MainActivity.kt
@@ -1,14 +1,15 @@
 package com.example.number_systems_converter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.number_systems_converter.databinding.ActivityMainBinding
 import java.util.Locale
 import kotlin.math.pow
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -23,37 +24,30 @@ class MainActivity : AppCompatActivity() {
 
         binding.equal.setOnClickListener {
             val number = binding.numberBox.text.toString()
-            convertNumbers(number)
+            if (binding.numberBox.text.toString().isEmpty()) {
+                Toast.makeText(this@MainActivity, "Please enter number", Toast.LENGTH_SHORT).show()
+            } else {
+                convertNumbers(number)
+            }
         }
     }
-    //The arrow will be modified later due to time constraints
-    val items1 = listOf("From                     v", "Binary", "Decimal", "Octal", "Hexadecimal")
-    val items2 = listOf("To                           v", "Binary", "Decimal", "Octal", "Hexadecimal")
 
+    private val items = listOf("Binary", "Decimal", "Octal", "Hexadecimal")
     private fun initSpinner() {
-        val fAdapter = ArrayAdapter(this,com.google.android.material.R.layout.support_simple_spinner_dropdown_item,items1)
-
-        val tAdapter = ArrayAdapter(this, com.google.android.material.R.layout.support_simple_spinner_dropdown_item, items2)
-
         binding.spinnerFrom.apply {
-            adapter = fAdapter
-            onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-                override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                    before = items1[p2]
-                }
-                override fun onNothingSelected(p0: AdapterView<*>?) {TODO("Not yet implemented")}
+            setItems(items)
+            setOnSpinnerItemSelectedListener<String> { _, _, _, newText ->
+                before = newText
             }
         }
         binding.spinnerTo.apply {
-            adapter = tAdapter
-            onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-                override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                    after = items2[p2]
-                }
-                override fun onNothingSelected(p0: AdapterView<*>?) {TODO("Not yet implemented")}
+            setItems(items)
+            setOnSpinnerItemSelectedListener<String> { _, _, _, newText ->
+                after = newText
             }
         }
     }
+
     private fun convertDecimalToBinary(decimalNumber: Int): Int {
         var dNumber = decimalNumber
         var binaryNumber = 0
@@ -67,6 +61,7 @@ class MainActivity : AppCompatActivity() {
         }
         return binaryNumber
     }
+
     private fun convertBinaryToDecimal(binaryNumber: Int): Int {
         var bNumber = binaryNumber
         var decimalNo = 0
@@ -83,18 +78,25 @@ class MainActivity : AppCompatActivity() {
         val decimal = HexaNumber.toLong(16)
         return decimal.toString(2)
     }
+
     private fun convertBinaryToHexadecimal(BinaryNumber: String): String {
         val decimal = BinaryNumber.toInt(2)
         return decimal.toString(16).uppercase(Locale.getDefault())
     }
+
     private fun convertDecimalToHexadecimal(DecimalNumber: Int): String {
         val bNumber = convertDecimalToBinary(DecimalNumber)
         return convertBinaryToHexadecimal(bNumber.toString())
     }
+
     private fun convertHexadecimalToDecimal(HexaNumber: String): Int {
-        val bNumber = convertHexadecimalToBinary(HexaNumber)
-        return convertBinaryToDecimal(bNumber.toInt())
+        var decimal = 0
+        for (digit in HexaNumber) {
+            decimal = decimal * 16 + Character.digit(digit, 16)
+        }
+        return decimal
     }
+
     private fun convertDecimalToOctal(DecimalNumber: Int): Long {
         var dNumber = DecimalNumber
         var octalNumber = 0
@@ -107,6 +109,7 @@ class MainActivity : AppCompatActivity() {
         }
         return octalNumber.toLong()
     }
+
     private fun convertOctalToDecimal(octalNumber: Long): Int {
         var decimal = 0L
         var oNumber = octalNumber
@@ -122,46 +125,54 @@ class MainActivity : AppCompatActivity() {
 
         return decimal.toInt()
     }
+
     private fun convertOctalToBinary(octalNumber: Long): Int {
         val decimalNumber = convertOctalToDecimal(octalNumber)
         return convertDecimalToBinary(decimalNumber)
     }
-    private fun convertBinaryToOctal(binaryNumber: Int):Long{
+
+    private fun convertBinaryToOctal(binaryNumber: Int): Long {
         val decimalNumber = convertBinaryToDecimal(binaryNumber)
         return convertDecimalToOctal(decimalNumber)
     }
-    private fun convertHexadecimalToOctal(HexaNumber: String):Long{
+
+    private fun convertHexadecimalToOctal(HexaNumber: String): Long {
         val decimalNumber = convertHexadecimalToDecimal(HexaNumber)
         return convertBinaryToOctal(decimalNumber)
     }
-    private fun convertOctalToHexadecimal(octalNumber: Long):String{
+
+    private fun convertOctalToHexadecimal(octalNumber: Long): String {
         val decimalNumber = convertOctalToDecimal(octalNumber)
         return convertDecimalToHexadecimal(decimalNumber)
     }
 
     // A function to ensure that the user input a valid value
-    private fun containLetters(n:String):Boolean{
-        return n.any { it.isLetter() }
-    }
-    private fun containsSymbols(str: String): Boolean {
-        val regex = Regex("[^A-Za-z0-9 ]")
-        return regex.containsMatchIn(str)
-    }
-    // hexa(symbols) && binary(0,1)
-    private fun convertNumbers(number: String) {
-        if(binding.numberBox.text.toString().isEmpty()){
-            Toast.makeText(this@MainActivity, "Please enter number", Toast.LENGTH_SHORT).show()
+    private fun checkInput(before: String?, n: String): Boolean {
+        if (before == "Binary" && n.any { it != '0' && it != '1' }) {
+            Toast.makeText(this, "This system only allows 0 and 1", Toast.LENGTH_SHORT).show()
+            return true
         }
-        if((before == "Binary" || before == "Decimal" || before == "Octal") && (containLetters(number) || containsSymbols(number)) ){
-            Toast.makeText(this, "This system only allows numbers",Toast.LENGTH_SHORT).show()
+        if ((before == "Decimal" || before == "Octal") && n.any { it.isLetter() }) {
+            Toast.makeText(this, "This system only allows numbers", Toast.LENGTH_SHORT).show()
+            return true
+        }
+        return false
+    }
+    // before = after with symbol && binary(0,1)
+    private fun convertNumbers(number: String) {
+        if (checkInput(before, number)) {
             binding.result.text = ""
         }
-        else{
-            if(binding.numberBox.text == null){
+        else {
+            if (binding.numberBox.text == null) {
                 Toast.makeText(this@MainActivity, "Please enter number", Toast.LENGTH_SHORT).show()
             }
-            if (before == items1[0] || after == items2[0]) {
-                Toast.makeText(this@MainActivity, "Please select remaining system", Toast.LENGTH_SHORT).show()
+            if (before == null || after == null) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Please select remaining system",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
             if (before == "Binary" && after == "Decimal") {
                 val result = convertBinaryToDecimal(number.toInt())
@@ -175,44 +186,47 @@ class MainActivity : AppCompatActivity() {
                 val result = convertHexadecimalToBinary(number)
                 binding.result.text = result
             }
-            if (before == "Binary" && after =="Hexadecimal") {
+            if (before == "Binary" && after == "Hexadecimal") {
                 val result = convertBinaryToHexadecimal(number)
                 binding.result.text = result
             }
-            if(before == "Hexadecimal" && after == "Decimal"){
+            if (before == "Hexadecimal" && after == "Decimal") {
                 val result = convertHexadecimalToDecimal(number)
                 binding.result.text = result.toString()
             }
-            if(before == "Decimal" && after == "Hexadecimal"){
+            if (before == "Decimal" && after == "Hexadecimal") {
                 val result = convertDecimalToHexadecimal(number.toInt())
                 binding.result.text = result
             }
-            if(before == "Decimal" && after == "Octal"){
+            if (before == "Decimal" && after == "Octal") {
                 val result = convertDecimalToOctal(number.toInt())
                 binding.result.text = result.toString()
             }
-            if(before == "Octal" && after == "Decimal"){
+            if (before == "Octal" && after == "Decimal") {
                 val result = convertOctalToDecimal(number.toLong())
                 binding.result.text = result.toString()
             }
-            if(before == "Octal" && after == "Binary"){
+            if (before == "Octal" && after == "Binary") {
                 val result = convertOctalToBinary(number.toLong())
                 binding.result.text = result.toString()
             }
-            if(before == "Binary" && after == "Octal"){
+            if (before == "Binary" && after == "Octal") {
                 val result = convertBinaryToOctal(number.toInt())
                 binding.result.text = result.toString()
             }
-            if(before == "Hexadecimal" && after == "Octal"){
+            if (before == "Hexadecimal" && after == "Octal") {
                 val result = convertHexadecimalToOctal(number)
                 binding.result.text = result.toString()
             }
-            if(before == "Octal" && after == "Hexadecimal"){
+            if (before == "Octal" && after == "Hexadecimal") {
                 val result = convertOctalToHexadecimal(number.toLong())
                 binding.result.text = result
             }
-            if(before == after){
-                Toast.makeText(this,"Both systems are the same, choose a different system!",Toast.LENGTH_SHORT).show()
+            if (before == after) {
+                Toast.makeText(this,
+                    "Both systems are the same, choose a different system!",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
     }
