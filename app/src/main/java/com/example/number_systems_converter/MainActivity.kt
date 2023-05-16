3617e795fca460006110cb5032bd3f95ed2919a7
Ali <alialashwall14@gmail.com>
2023-05-17 02:45:24 +0300

    main commit

--- app/src/main/java/com/example/number_systems_converter/MainActivity.kt
+++ app/src/main/java/com/example/number_systems_converter/MainActivity.kt
@@ -10,7 +10,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     var before: String? = null
     var after: String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -26,7 +25,6 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-
     private val items = listOf("Binary", "Decimal", "Octal", "Hexadecimal")
     private fun initSpinner() {
         binding.spinnerFrom.apply {
