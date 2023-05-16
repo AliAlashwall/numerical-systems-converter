94498d63678959edb6799d8e54fb9d84a336421a
Ali <alialashwall14@gmail.com>
2023-05-17 02:32:39 +0300

    main commit

--- app/src/main/java/com/example/number_systems_converter/MainActivity.kt
+++ app/src/main/java/com/example/number_systems_converter/MainActivity.kt
@@ -1,15 +1,10 @@
 package com.example.number_systems_converter
 
 import android.os.Bundle
-import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.number_systems_converter.databinding.ActivityMainBinding
 import java.util.Locale
-import kotlin.math.pow
-
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -47,105 +42,48 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-
     private fun convertDecimalToBinary(decimalNumber: Int): Int {
-        var dNumber = decimalNumber
-        var binaryNumber = 0
-        var remainder: Int
-        var i = 1
-        while (dNumber != 0) {
-            remainder = dNumber % 2
-            dNumber /= 2
-            binaryNumber += (remainder * i)
-            i *= 10
-        }
-        return binaryNumber
+        return Integer.toBinaryString(decimalNumber).toInt()
     }
-
     private fun convertBinaryToDecimal(binaryNumber: Int): Int {
-        var bNumber = binaryNumber
-        var decimalNo = 0
-        var power = 0
-        while (bNumber > 0) {
-            val r = bNumber % 10
-            decimalNo = (decimalNo + r * 2.0.pow(power.toDouble())).toInt()
-            bNumber /= 10
-            power++
-        }
-        return decimalNo
+        return Integer.parseInt(binaryNumber.toString(),2)
     }
     private fun convertHexadecimalToBinary(HexaNumber: String): String {
-        val decimal = HexaNumber.toLong(16)
-        return decimal.toString(2)
+        val decimal =convertHexadecimalToDecimal(HexaNumber)
+        return convertDecimalToHexadecimal(decimal)
     }
-
     private fun convertBinaryToHexadecimal(BinaryNumber: String): String {
-        val decimal = BinaryNumber.toInt(2)
-        return decimal.toString(16).uppercase(Locale.getDefault())
+        val decimal =convertBinaryToDecimal(BinaryNumber.toInt())
+        return convertDecimalToHexadecimal(decimal)
     }
-
     private fun convertDecimalToHexadecimal(DecimalNumber: Int): String {
-        val bNumber = convertDecimalToBinary(DecimalNumber)
-        return convertBinaryToHexadecimal(bNumber.toString())
+        return Integer.toHexString(DecimalNumber).uppercase(Locale.ROOT)
     }
-
     private fun convertHexadecimalToDecimal(HexaNumber: String): Int {
-        var decimal = 0
-        for (digit in HexaNumber) {
-            decimal = decimal * 16 + Character.digit(digit, 16)
-        }
-        return decimal
+        return Integer.parseInt(HexaNumber,16)
     }
-
     private fun convertDecimalToOctal(DecimalNumber: Int): Long {
-        var dNumber = DecimalNumber
-        var octalNumber = 0
-        var i = 1
-
-        while (dNumber != 0) {
-            octalNumber += (dNumber % 8) * i
-            dNumber /= 8
-            i *= 10
-        }
-        return octalNumber.toLong()
+        return Integer.toOctalString(DecimalNumber).toLong()
     }
-
     private fun convertOctalToDecimal(octalNumber: Long): Int {
-        var decimal = 0L
-        var oNumber = octalNumber
-        var i = 0
-        var rem: Long
-
-        while (oNumber != 0L) {
-            rem = oNumber % 10
-            decimal += rem * 8.0.pow(i.toDouble()).toLong()
-            ++i
-            oNumber /= 10
-        }
-
-        return decimal.toInt()
+    return Integer.parseInt(octalNumber.toString(),8)
     }
-
     private fun convertOctalToBinary(octalNumber: Long): Int {
         val decimalNumber = convertOctalToDecimal(octalNumber)
         return convertDecimalToBinary(decimalNumber)
     }
-
     private fun convertBinaryToOctal(binaryNumber: Int): Long {
         val decimalNumber = convertBinaryToDecimal(binaryNumber)
         return convertDecimalToOctal(decimalNumber)
     }
-
     private fun convertHexadecimalToOctal(HexaNumber: String): Long {
         val decimalNumber = convertHexadecimalToDecimal(HexaNumber)
-        return convertBinaryToOctal(decimalNumber)
+        return convertDecimalToOctal(decimalNumber)
     }
-
     private fun convertOctalToHexadecimal(octalNumber: Long): String {
         val decimalNumber = convertOctalToDecimal(octalNumber)
         return convertDecimalToHexadecimal(decimalNumber)
     }
-
     // A function to ensure that the user input a valid value
     private fun checkInput(before: String?, n: String): Boolean {
         if (before == "Binary" && n.any { it != '0' && it != '1' }) {
